40bf049ade2aece920befe6f02eb762907d0809a
Aananditaa <122295513+Tech-neophyte@users.noreply.github.com>
2024-04-23 04:37:46 +0530

    Update Footer.js

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,32 +4,32 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <>
-      <div className="created-by">Made with ❤️ by Hrishabh Jain</div>
+      <div className="created-by">Hope you enjoy it!</div>
       <div className="contact">
         <a
           rel="noopener noreferrer"
-          href="https://www.instagram.com/hrishabh.hj/"
+          href="https://www.instagram.com/"
           target="_blank"
         >
           <i class="fa-brands fa-instagram"></i>
         </a>
         <a
           rel="noopener noreferrer"
-          href="https://twitter.com/hrishabhcodes"
+          href="https://twitter.com/CompSciFact?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"
           target="_blank"
         >
           <i class="fa-brands fa-twitter"></i>
         </a>
         <a
           rel="noopener noreferrer"
-          href="https://github.com/HrishabhCodes"
+          href="https://github.com/Tech-neophyte/VistaJist"
           target="_blank"
         >
           <i class="fa-brands fa-github"></i>
         </a>
         <a
           rel="noopener noreferrer"
-          href="https://www.linkedin.com/in/hrishabh-jain/"
+          href="https://www.linkedin.com/"
           target="_blank"
         >
           <i class="fa-brands fa-linkedin"></i>
